3bbc17925648844f1bfb77b06d1fbba43dd75418
nivriii <nivri.learning@gmail.com>
2022-08-09 20:52:04 -0500

    create dws-service proyect

---
+++ dws-service/src/main/java/com/master/microservices/dwsservice/DwsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.master.microservices.dwsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwsServiceApplication.class, args);
+	}
+
+}
